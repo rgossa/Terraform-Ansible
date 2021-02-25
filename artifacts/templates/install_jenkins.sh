ef0ea3e956153d91140dcab1fc9c8c32104dddca
rgossa <github@gossa.eu>
2021-02-25 16:08:25 +0100

    Migrate Java to OpenJDK

--- artifacts/templates/install_jenkins.sh
+++ artifacts/templates/install_jenkins.sh
@@ -2,11 +2,8 @@
 
 sudo apt-get install language-pack-en -y
 
-sudo apt-get install -y python-software-properties debconf-utils
-sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
+sudo apt-get install openjdk-8-jdk -y
 
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo apt-add-repository "deb https://pkg.jenkins.io/debian-stable binary/"
